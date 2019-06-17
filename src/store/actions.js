ac567e375f21b9af88c13848593a4883693af7a5
davieu <davisumana@gmail.com>
2019-06-17 01:32:10 -0400

    add a pet through the form

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,9 @@
 export default {
-  
+  // addPet: (context, pet) => {
+  //   context.commit('appendPet', pet)
+  // }
+  //Both are the same bottom is the standard. {commit} is from vuex
+  addPet: ({ commit }, payload) => {
+    commit('appendPet', payload)
+  }
 }
