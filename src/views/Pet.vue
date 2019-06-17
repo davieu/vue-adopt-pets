169c0a79916aa1a4c3db44ebddee2d5153bac571
davieu <davisumana@gmail.com>
2019-06-16 20:27:27 -0400

    added vuex state management and moved the data for the cats and dogs in its store also pet view displays the data of the specific pet clicked

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
-    <h1>PETTTS</h1>
+    <h1>{{ animal.name }} ({{ $route.params.species.substring(3,0) }})</h1>
+    <p>Age: {{ animal.age }} years old</p>
+    <p>Breed: {{ animal.breed }}</p>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return{
+      animal: []
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats', 
+      'dogs'
+    ])
+    // cats() {
+    //   return this.$store.state.cats
+    // }
+  },
+  mounted() {
+    //this['cats'][0] is what it looks like. To access the state for the specific animal 
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+</script>
+
